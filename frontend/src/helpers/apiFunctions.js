bcb526c2c7e8e8b26c7e2599a7615652c16eec7f
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-03 10:10:28 -0300

    return data

--- frontend/src/helpers/apiFunctions.js
+++ frontend/src/helpers/apiFunctions.js
@@ -4,28 +4,28 @@ const baseUrl = 'http://localhost:3001/tasks';
 
 export const getAll = async () => {
   const response = await axios.get(baseUrl);
-  console.log(response);
-  return response;
+  // console.log(response);
+  return response.data;
 };
 
-export const post = async (title) => {
+export const post = async ({ title }) => {
   const body = {
     title,
     type: 'a fazer',
   };
   const response = await axios.post(baseUrl, body);
-  console.log(response);
+  // console.log(response);
   return response;
 };
 
 export const put = async (id, type) => {
   const response = await axios.put(`${baseUrl}/${id}`, { type });
-  console.log(response);
+  // console.log(response);
   return response;
 };
 
 export const destroy = async (id) => {
-  const response = await axios.destroy(`${baseUrl}/${id}`);
-  console.log(response);
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  // console.log(response);
   return response;
 };
